06908f61bf939555f1673bbda96104b74dd353de
korynewton <korynewtondev@gmail.com>
2019-03-28 15:54:32 -0700

    get related recipes by dish style

--- dishes/dishes-model.js
+++ dishes/dishes-model.js
@@ -10,14 +10,13 @@ function getDishes() {
   return db('dishes');
 }
 
-function getDish(id) {
-  return db('dishes')
-    .where({ id })
-    .first();
-}
-
 function addDish(dish) {
   return db('dishes')
     .insert(dish)
     .then(ids => findById(ids[0]));
 }
+
+function getDish(id) {
+  return db('recipestyles')
+    .where({dish_id: id})
+};
--- dishes/dishes-routes.js
+++ dishes/dishes-routes.js
@@ -6,22 +6,22 @@ const Dishes =  require('./dishes-model')
 router.get('/', async (req, res) => {
     // get dishes from the database
     try {
-      const dishes = await Dishes.getDishes();
+      const dishes = await Dishes.getDished();
       res.status(200).json(dishes);
     } catch (error) {
       res.status(500).json(error);
     }
   });
 
-router.get('/:id', async (req, res) => {
-  // get the dishes from the database
-  try {
-    const dishes = await Dishes.getDish(req.params.id);
-    res.status(200).json(dishes);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+// router.get('/:id', async (req, res) => {
+//   // get the dishes from the database
+//   try {
+//     const dishes = await Dishes.getDish(req.params.id);
+//     res.status(200).json(dishes);
+//   } catch (error) {
+//     res.status(500).json(error);
+//   }
+// });
 
 router.post('/', async (req, res) => {
   try {
@@ -32,5 +32,14 @@ router.post('/', async (req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  // get the dishes from the database
+  try {
+    const dishes = await Dishes.getDish(req.params.id);
+    res.status(200).json(dishes);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
