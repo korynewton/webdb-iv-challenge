a13fda81781c81d42dbe08fd04697889cd5bae42
korynewton <korynewtondev@gmail.com>
2019-03-28 18:11:54 -0700

    getDish updated

--- dishes/dishes-model.js
+++ dishes/dishes-model.js
@@ -16,7 +16,23 @@ function addDish(dish) {
     .then(ids => findById(ids[0]));
 }
 
+// function getDish(id) {
+//   return db('recipestyles')
+//     .where({dish_id: id})
+// };
+
 function getDish(id) {
-  return db('recipestyles')
-    .where({dish_id: id})
-};
+  return db('dishes')
+    .innerJoin('recipe_styles', 'dishes.id', '=', 'recipe_styles.id')
+    .innerJoin('recipes', 'recipe_styles.id', '=', 'recipes.id')
+    .select('recipes.name')
+    .where({ 'recipe_styles.dish_id': id })
+    .then(recipes => {
+      return db('dishes')
+        .where({ id })
+        .first()
+        .then(dish => {
+          return { ...dish, recipes };
+        });
+    });
+}
